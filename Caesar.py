129540839c7f3fa8eafeb457b13d88632af47526
Sofia Sackett <43033791+sofiasackett@users.noreply.github.com>
2019-02-06 12:56:32 -0500

    Update Caesar.py

--- Caesar.py
+++ Caesar.py
@@ -5,8 +5,8 @@
 import string
 
 # Input
-hiddenText = input("Enter the text that you would like encrypted: ")
-hiddenText = hiddenText.lower()
+plaintext = input("Enter the text that you would like encrypted: ")
+plaintext = plaintext.lower()
 shift = int(input("Enter your desired shift: "))
 
 # Function
@@ -15,7 +15,7 @@ def caesar (text, shift, alphabet=string.ascii_lowercase):
     return text.translate(text.maketrans(alphabet, alphaShift))
 
 # Output
-print (caesar(hiddenText, shift))
+print (caesar(plaintext, shift))
 
 
 
