82cde25291ea87afebd8789411e490f7eea60d73
Sofia Sackett <43033791+sofiasackett@users.noreply.github.com>
2019-02-06 13:00:35 -0500

    Add files via upload
    Atbash is a substitution cipher where the letters of the English alphabet are reversed. Obviously, it is extremely easy to crack (it can also be thought of as an Affine cipher where a = 25 and b = 25. Future improvements should include a decryption function.

---
+++ Atbash.py
@@ -0,0 +1,17 @@
+# Sofia Sackett
+# Atbash Cipher
+# Reversed alphabet
+
+# Import
+import string
+
+# Input
+hiddenText  = input("Enter the text that you would like encrypted: ")
+
+# Function
+def atbash (text, alphabet = string.ascii_lowercase):
+    atbashAlpha = alphabet[::-1]
+    return text.translate(text.maketrans(alphabet, atbashAlpha))
+
+# Output
+print (atbash(hiddenText))
