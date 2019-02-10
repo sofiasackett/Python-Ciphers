829b5529f5296a7d7f3d4e3bf3016bdf48c7f4ec
Sofia Sackett <43033791+sofiasackett@users.noreply.github.com>
2019-02-10 18:52:28 -0500

    Update Affine.py
    Decryption function added and cleanup. Future improvements = extending m to include uppercase alphabet and symbols/spacing?

--- Affine.py
+++ Affine.py
@@ -1,39 +1,62 @@
 # Sofia Sackett
 #Affine Cipher
 
-# Set up
 import string
 alphabet = list (map(chr, range(ord('a'), ord('z') + 1)))
 
-# Directions
-print("The Affine Cipher uses an encryption scheme based on three values, a and b")
-print ("The variables a and b represent two numbers that must each be less than m and greater than 1.")
-print("The first number, a, should have no factors in common with m.")
-
-# Input
-varA = int(input("Enter a value for variable a: "))
-varB = int(input("Enter a value for variable b: "))
-plaintext = input("Enter the text you would like encrypted: ")
-plaintext = plaintext.replace(" ", "")
-m = 26
-
-# Function
-def affine (a, b, plaintext):
-    cipherText = ""
+def main():
+    question = print("Enter 'e' to encipher or 'd' to decipher.")
+    mode = input("Choose mode: ")
+    m = 26
     
+    if (mode == 'e' or mode == 'E'):
+        varA = int(input("Enter a value for variable a: "))
+        varB = int(input("Enter a value for variable b: "))
+        plaintext = input("Enter the text you would like enciphered: ")
+        plaintext = plaintext.replace(" ", "")
+        plaintext = plaintext.lower()
+        print (encipher(varA, varB, m, plaintext))
+        
+    elif (mode == 'd' or mode == 'D'):
+        varA = int(input("Enter the key for variable a: "))
+        varB = int(input("Enter the key for variable b: "))
+        ciphertext = input("Enter the text you would like deciphered: ")
+        ciphertext = ciphertext.replace(" ", "")
+        ciphertext = ciphertext.lower()
+        print (decipher(varA, varB, m, ciphertext))
+
+    else:
+        print("An error occured. Please enter 'e' to encipher or 'd' to decipher.")
+
+def encipher (a, b, m, plaintext):
+    ciphertext = ""
     for letter in plaintext:
         p = letter
         p = alphabet.index(letter)
-
         c = ((a*p) + b) % m
         cIndex = c % len(alphabet)
         cCharacter = alphabet[cIndex]
+        ciphertext += cCharacter
+    return ciphertext
+       
+def find_mod_inverse(a, m):
+    a = a % m
+    for x in range (1, m):
+        if ((a * x) % m == 1):
+            return x
         
-        cipherText += cCharacter
+def decipher(a, b, m, ciphertext):
+    plaintext = ""
+    for letter in ciphertext:
+        c = letter
+        c = alphabet.index(letter)
+        x = find_mod_inverse(a, 26)
+        p = x * (c-b) % m
+        pIndex = p % len(alphabet)
+        pCharacter = alphabet[pIndex]
+        plaintext += pCharacter
+    return plaintext
 
-    return cipherText
+main()
 
-
-# Output
-print (affine(varA, varB, plaintext))
     
