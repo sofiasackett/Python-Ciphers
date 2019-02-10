5cc3335426713b749e3008877aa9a3de6cee95ac
Sofia Sackett <43033791+sofiasackett@users.noreply.github.com>
2019-02-10 12:37:04 -0500

    Update ROT13.py
    Decryption function added

--- ROT13.py
+++ ROT13.py
@@ -1,19 +1,33 @@
 # Sofia Sackett
 # ROT13 Cipher
 
-# Import
 import string
 
-# Input
-plaintext = input("Enter the text that you would like encrypted: ")
-plaintext = plaintext.lower()
-plaintext = plaintext.replace(" ", "")
+question = print("Enter 'e' to encipher or 'd' to decipher.")
+mode = input("Choose mode: ")
 
+alphabet = string.ascii_lowercase
+rot13Alpha = alphabet[13:] + alphabet[:13]
 
-# Function
-def rot13 (text, alphabet=string.ascii_lowercase):
-    rot13Alpha = alphabet[13:] + alphabet[:13]
-    return text.translate(text.maketrans(alphabet, rot13Alpha))
+if mode == 'e' or mode == 'E':
+    plaintext = input("Enter the text that you would like encrypted: ")
+    plaintext = plaintext.lower()
+    plaintext = plaintext.replace(" ", "")
 
-# Output
-print (rot13(plaintext))
+    def encipher (text, alphabet):
+        return text.translate(text.maketrans(alphabet, rot13Alpha))
+
+    print (encipher(plaintext, alphabet))
+
+elif mode == "d" or mode == 'D':
+    ciphertext = input("Enter the text that you would like deciphered: ")
+    ciphertext = ciphertext.lower()
+    ciphertext = ciphertext.replace(" ", "")
+
+    def decipher (text, alphabet):
+        return text.translate(text.maketrans(rot13Alpha, alphabet))
+
+    print (decipher(ciphertext, alphabet))
+
+else:
+    print("An error occured. Please enter 'e' to encipher or 'd' to decipher.")
