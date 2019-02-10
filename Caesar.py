f7040b65107c2ed091dec68fcfcbc000560718e3
Sofia Sackett <43033791+sofiasackett@users.noreply.github.com>
2019-02-10 12:30:05 -0500

    Update Caesar.py
    Decryption function added

--- Caesar.py
+++ Caesar.py
@@ -1,22 +1,40 @@
 # Sofia Sackett
 # Caesar Cipher
 
-# Import
 import string
 
-# Input
-plaintext = input("Enter the text that you would like encrypted: ")
-plaintext = plaintext.lower()
-plaintext = plaintext.replace(" ", "")
-shift = int(input("Enter your desired shift: "))
+question = print("Enter 'e' to encipher or 'd' to decipher.")
+mode = input("Choose mode: ")
 
-# Function
-def cipher (text, shift, alphabet=string.ascii_lowercase):
-    alphaShift = alphabet[shift:] + alphabet[:shift]
-    return text.translate(text.maketrans(alphabet, alphaShift))
+# Encode
+if mode == 'e' or mode == 'E':
+    plaintext = input("Enter the text that you would like enciphered: ")
+    plaintext = plaintext.lower()
+    plaintext = plaintext.replace(" ", "")
+    shift = int(input("Enter your desired shift: "))
 
-# Output
-print (cipher(plaintext, shift))
+    def encipher (text, shift, alphabet=string.ascii_lowercase):
+        alphaShift = alphabet[shift:] + alphabet[:shift]
+        return text.translate(text.maketrans(alphabet, alphaShift))
+
+    print (encipher(plaintext, shift))
+
+# Decode
+elif mode == "d" or mode == 'D':
+    ciphertext = input("Enter the text that you would like deciphered: ")
+    ciphertext = ciphertext.lower()
+    ciphertext = ciphertext.replace(" ", "")
+    key = int(input("Enter the shift key: "))
+        
+    def decipher (ciphertext, key, alphabet=string.ascii_lowercase):
+        keyShift = alphabet[key:] + alphabet[:key]
+        return ciphertext.translate(ciphertext.maketrans(keyShift, alphabet))
+
+    print(decipher(ciphertext, key))
+
+else:
+    print("An error occured. Please enter either 'e' to encipher or 'd' to decipher.")
+        
 
 
 
